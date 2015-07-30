336291e9f79a6c2e219f83602b3dbae2e6265c12
hathbanger <hathbanger.ah@gmail.com>
2015-07-30 09:19:26 -0600

    First Commit

---
+++ db/migrate/20150729153628_create_tricks.rb
@@ -0,0 +1,10 @@
+class CreateTricks < ActiveRecord::Migration
+  def change
+    create_table :tricks do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
