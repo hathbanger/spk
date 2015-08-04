a2de5a2d34c309e72c0a9e4a302ea388bda1ffae
hathbanger <hathbanger.ah@gmail.com>
2015-08-04 08:52:45 -0600

    bugs

--- app/assets/javascripts/geo.js
+++ app/assets/javascripts/geo.js
@@ -1,91 +1,91 @@
 
 
-function getLocation() {
-	navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude);
-  	if(position.coords.latitude){
-  		$('#trick_lati').val(position.coords.latitude)
-  		$('#trick_long').val(position.coords.longitude)
-  	}
+// function getLocation() {
+// 	navigator.geolocation.getCurrentPosition(function(position) {
+//   console.log(position.coords.latitude, position.coords.longitude);
+//   	if(position.coords.latitude){
+//   		$('#trick_lati').val(position.coords.latitude)
+//   		$('#trick_long').val(position.coords.longitude)
+//   	}
 
-		});
-}
+// 		});
+// }
 
 
-function showPosition(position) {
-    $('#demo').text("Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude);
-    }	
+// function showPosition(position) {
+//     $('#demo').text("Latitude: " + position.coords.latitude + 
+//     "<br>Longitude: " + position.coords.longitude);
+//     }	
 
-  jQuery(function($) {
-    // Asynchronously Load the map API 
+//   jQuery(function($) {
+//     // Asynchronously Load the map API 
 
-    var script = document.createElement('script');
-    script.src = "https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-    document.body.appendChild(script);
+//     var script = document.createElement('script');
+//     script.src = "https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+//     document.body.appendChild(script);
 
-});
+// });
 
-function initialize() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
+// function initialize() {
+//     var map;
+//     var bounds = new google.maps.LatLngBounds();
+//     var mapOptions = {
+//         mapTypeId: 'roadmap'
+//     };
                     
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.setTilt(45);
+//     // Display a map on the page
+//     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+//     map.setTilt(45);
         
-    // Multiple Markers
-    var lat = $('.coords:eq(0)').text();
-    var lon = $('.coords:eq(1)').text();
+//     // Multiple Markers
+//     var lat = $('.coords:eq(0)').text();
+//     var lon = $('.coords:eq(1)').text();
 
-    var markers = [
-        ['Broomfield Skatepark, Broomfield', 39.9166938,-105.0627738],
-        ['Denver Skatepark, Denver', 39.7596167,-105.0025979]
-    ];
+//     var markers = [
+//         ['Broomfield Skatepark, Broomfield', 39.9166938,-105.0627738],
+//         ['Denver Skatepark, Denver', 39.7596167,-105.0025979]
+//     ];
                         
-    // Info Window Content
-    var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>Boulder Skatepark</h3>' +
-        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
-        ['<div class="info_content">' +
-        '<h3>Denver Skatepark</h3>' +
-        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-        '</div>']
-    ];
+//     // Info Window Content
+//     var infoWindowContent = [
+//         ['<div class="info_content">' +
+//         '<h3>Boulder Skatepark</h3>' +
+//         '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
+//         ['<div class="info_content">' +
+//         '<h3>Denver Skatepark</h3>' +
+//         '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
+//         '</div>']
+//     ];
         
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
+//     // Display multiple markers on a map
+//     var infoWindow = new google.maps.InfoWindow(), marker, i;
     
-    // Loop through our array of markers & place each one on the map  
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
+//     // Loop through our array of markers & place each one on the map  
+//     for( i = 0; i < markers.length; i++ ) {
+//         var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+//         bounds.extend(position);
+//         marker = new google.maps.Marker({
+//             position: position,
+//             map: map,
+//             title: markers[i][0]
+//         });
         
-        // Allow each marker to have an info window    
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
+//         // Allow each marker to have an info window    
+//         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//             return function() {
+//                 infoWindow.setContent(infoWindowContent[i][0]);
+//                 infoWindow.open(map, marker);
+//             }
+//         })(marker, i));
 
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
+//         // Automatically center the map fitting all markers on the screen
+//         map.fitBounds(bounds);
+//     }
 
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(11);
-        google.maps.event.removeListener(boundsListener);
-    });
+//     // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+//     var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+//         this.setZoom(11);
+//         google.maps.event.removeListener(boundsListener);
+//     });
     
-}
+// }
