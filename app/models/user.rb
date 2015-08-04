b8ddad735557046c9d29c147c1d0f87430530631
hathbanger <hathbanger.ah@gmail.com>
2015-08-04 16:38:26 -0600

    more interlinking

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
    has_many :comments
    acts_as_followable
    acts_as_follower
-    has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }
+    has_attached_file :avatar, styles: { medium: "300x300#", thumb: "50x50#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
